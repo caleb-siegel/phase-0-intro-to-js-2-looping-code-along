e4b6d61abe1c98efb648ac8a92f2f89127917a83
Caleb Siegel <caleb.siegel@gmail.com>
2023-12-12 21:26:50 -0500

    Completed lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+
+const names = ["Guadalupe", "Ollie", "Aki"]
+
+function writeCards (names, event) {
+    const thankYous = []
+    for (let i = 0; i < names.length; i++) {
+        thankYous.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+        console.log(i)
+    }
+    return thankYous
+}
+
+writeCards(names, "Birthday")
+
+function countDown () {
+for (let countDown = 10; countDown >= 0; countDown --) {
+    console.log(countDown)
+}
+}
+
+countDown ()
